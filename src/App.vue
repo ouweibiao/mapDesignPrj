1bf051144c7c9d3eab3f3494be78129503230e05
Sakura <1228743418@qq.com>
2023-08-26 22:01:37 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+//import { UploadOutlined } from '@ant-design/icons-vue';
+import Frame from './components/Frame.vue'  //整体的框架组件
+
+</script>
+
+<template>
+    <Frame></Frame>
+
+
+    <!--绘制文件上传按钮
+    <a-upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" directory>
+      <a-button>
+        <upload-outlined></upload-outlined>
+        Upload Directory
+      </a-button>
+    </a-upload>-->
+</template>
---
+++ src/components/Map.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import mapboxgl from 'mapbox-gl'
+import MapboxLanguage from '@mapbox/mapbox-gl-language'
+
+let map
+const initMap = () => {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmRlcm1hbm4iLCJhIjoiY2xramlxbW5xMHB6OTN0cWx2YXdyOWMyNiJ9.11h9QepknHdlVU2j-0r5EA';
+  Map = new mapboxgl.Map({
+    container: 'map', // container id 绑定的组件的id
+    style: 'mapbox://styles/mapbox/streets-v11', //地图样式，可以使用官网预定义的样式,也可以自定义
+    center: [115.8574663, 28.65622505], // 初始地图中心点位置
+    zoom: 11,     // starting zoom 地图初始的层级
+    antialias: true, //抗锯齿，通过false关闭提升性能
+  });
+  Map.addControl(new MapboxLanguage({ defaultLanguage: 'zh-Hans' }))//切换成中文
+}
+
+
+
+onMounted(() => {
+  initMap()
+})
+
+
+
+</script>
+
+
+
+
