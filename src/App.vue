ef19417c91fa551a0b6d5a2fd58d66222143045f
MLin150 <2721266256@qq.com>
2023-08-30 18:15:40 +0800

    add all

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 //import { UploadOutlined } from '@ant-design/icons-vue';
-import Frame from './components/Frame.vue'  //整体的框架组件
-
+// import Frame from './components/Frame.vue'  //整体的框架组件
+import BaiduMap from './components/BaiduMap.vue'
 </script>
 
 <template>
-    <Frame></Frame>
-
+    <!-- <Frame></Frame> -->
+     <BaiduMap></BaiduMap>
 
     <!--绘制文件上传按钮
     <a-upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" directory>
