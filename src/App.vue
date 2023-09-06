7db2607930f3058fd66e3ee98c40217d0f74ff55
MLin150 <2721266256@qq.com>
2023-09-06 09:51:48 +0800

    mapbox绘制交通灯

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 //import { UploadOutlined } from '@ant-design/icons-vue';
 // import Frame from './components/Frame.vue'  //整体的框架组件
 import BaiduMap from './components/BaiduMap.vue'
+import MapBox from './components/MapBox.vue'
+import Map from './components/Map.vue'
 </script>
 
 <template>
     <!-- <Frame></Frame> -->
-     <BaiduMap></BaiduMap>
-
+     <!-- <BaiduMap></BaiduMap> -->
+      <MapBox></MapBox>
+      <!-- <Map></Map> -->
     <!--绘制文件上传按钮
     <a-upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" directory>
       <a-button>
