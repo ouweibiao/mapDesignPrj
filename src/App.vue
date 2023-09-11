079b167642869998c93da719d964c8e89ed5c858
zhaowendao123456 <lhy1139398521@163.com>
2023-09-11 17:09:04 +0800

    public/favicon.ico: convert to Git LFS

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <script setup>
-//import { UploadOutlined } from '@ant-design/icons-vue';
-// import Frame from './components/Frame.vue'  //整体的框架组件
-import BaiduMap from './components/BaiduMap.vue'
+import MapApp from './components/MapApp.vue'
 </script>
 
 <template>
-    <!-- <Frame></Frame> -->
-     <BaiduMap></BaiduMap>
-
-    <!--绘制文件上传按钮
-    <a-upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" directory>
-      <a-button>
-        <upload-outlined></upload-outlined>
-        Upload Directory
-      </a-button>
-    </a-upload>-->
+  <MapApp/>
 </template>
