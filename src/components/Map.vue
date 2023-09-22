269ca10f1cb8de84aea81102e29ddadd3f807b9d
Sakura <1228743418@qq.com>
2023-09-22 12:34:29 +0800

    增加路灯

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -13,9 +13,12 @@ import { onMounted, ref } from 'vue';
 import mapboxgl from 'mapbox-gl';
 import MapboxLanguage from '@mapbox/mapbox-gl-language';
 import road from '../store/log/roadinfo.json';
-import {addTrafficLightLayers} from '@/utils/imageLoader';
 
 
+// import { addTrafficLightLayers } from '@/utils/imageLoader';
+
+import light from '../store/log/lightinfo.json'
+import time1 from '../store/log/time0.json'
 
 const position = ref([]);
 const carTrace = ref([]);
@@ -54,7 +57,7 @@ const createMap = () => {
   map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [114.08026355,22.5471340316],
+    center: [114.08026355, 22.5471340316],
     zoom: 16,
     antialias: true,
     lineCoordinates: road,
@@ -88,8 +91,32 @@ const createMap = () => {
   });
   map.addControl(new MapboxLanguage({ defaultLanguage: 'zh-Hans' }));
   // 增加交通灯图层并更新
-  logoPosition:"bottom-right"   //右下角
+  logoPosition: "bottom-right"   //右下角
 };
+
+
+
+
+
+
+
+
+
+
+
+function change() {//根据交通灯的坐标生成添加图像的四个坐标
+  var data_total = [];//存储添加图像的四个坐标
+  for (var i = 0; i < light.length; i++) {
+    var temp = [[light[i][0] - 0.00005, light[i][1] + 0.00006],
+      [light[i][0] + 0.00005, light[i][1] + 0.00006],
+      [light[i][0] + 0.00005, light[i][1] - 0.00006],
+      [light[i][0] - 0.00005, light[i][1] - 0.00006]];
+    data_total.push(temp);
+  }
+  console.log(data_total);
+  return data_total;
+}
+
 
 const removeLayerAndSource = id => {
   if (map.getLayer(id)) {
@@ -243,33 +270,108 @@ onMounted(() => {
   // 创建地图
   createMap();
 
-      map.on('mouseenter', 'places', (e) => {
-        map.getCanvas().style.cursor = 'pointer';
+
+
+  //存储照片URL的数组
+  var UrlArr = ['https://s2.loli.net/2023/09/05/o7jfui26X1ND4Ga.png',
+    'https://s2.loli.net/2023/09/05/n4tB5xXMzgecL7S.png',
+    'https://s2.loli.net/2023/09/05/2vM9Jz8huPRwNjF.png',
+    'https://s2.loli.net/2023/09/05/ixZ7uIEOzj1BJgr.png',
+    'https://s2.loli.net/2023/09/05/eN6w5JUkPDd1qEO.png',
+    'https://s2.loli.net/2023/09/05/sfGPgymTbSALYz5.png',
+    'https://s2.loli.net/2023/09/05/kj2oyMHn1YpuKCr.png',
+    'https://s2.loli.net/2023/09/05/qHahvDXzZtLpBGV.png',
+    'https://s2.loli.net/2023/09/05/EbdfUct1zyMQeWr.png'];
+
+  var data_total = change();
+  // var map = Init_Map();
+  map.on('load', () => {//当地图加载后执行
+    //先加载第0秒的交通灯状态展示在地图上
+    for (var i = 0; i < light.length; i++) {
+      //每个交通灯新建一个图层
+      var sourceid = 'radar' + i;
+      var layerid = 'radar-layer' + i;
+      //为每个交通灯的图片展示添加数据源，以sourceid为该源的id
+      map.addSource(sourceid, {
+        'type': 'image',
+        'url': UrlArr[time1[1][i]],
+        // 'url':'../images/2左转.jpeg',
+        'coordinates': data_total[i]
       });
-      map.on('mouseleave', 'places', () => {
-        map.getCanvas().style.cursor = '';
-      });
-      map.on('click', 'places', (e) => {
-        const clickedFeature = e.features[0];
-        clickedId.value = clickedFeature.properties.id.slice(1, -1);
-        clickFlag = !clickFlag;
-        if (clickFlag) {
-          pointColor.value = ('#778899');
-          updateMap();
-          drawTrace(clickedId.value);
-        } else {
-          removeLayerAndSource('route');
-          removeLayerAndSource('oneCar');
-          pointColor.value = ('#4264fb');
-          updateMap();
+      //利用上述的数据源，新增一个图层
+      map.addLayer({
+        id: layerid,
+        'type': 'raster',
+        'source': sourceid,
+        'paint': {
+          'raster-fade-duration': 0
         }
       });
-    })
+
+    }
+
+    var timecount = 1;//记录当前秒数，用于文件读取的文件名
+    setInterval(() => {//每隔一秒，读取一个文件,然后更新图层
+      var fileName = '../store/log/time' + timecount + '.json';
+      timecount++;
+      import(fileName)
+          .then((module) => {//module.default是每个文件中数据
+            for (var i = 0; i < light.length; i++) {
+              var sourceid = 'radar' + i;
+              //更新图像地址url，(可选更新位置coordinates),网址 https://docs.mapbox.com/mapbox-gl-js/api/sources/#imagesource#updateimage
+              map.getSource(sourceid).updateImage({ url: UrlArr[module.default[1][i]], });
+            }
+          })
+          .catch((error) => {
+            console.error(`Failed to import new file${fileName}:`, error);
+          });
+    }, 1000);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  map.on('mouseenter', 'places', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', 'places', () => {
+    map.getCanvas().style.cursor = '';
+  });
+  map.on('click', 'places', (e) => {
+    const clickedFeature = e.features[0];
+    clickedId.value = clickedFeature.properties.id.slice(1, -1);
+    clickFlag = !clickFlag;
+    if (clickFlag) {
+      pointColor.value = ('#778899');
+      updateMap();
+      drawTrace(clickedId.value);
+    } else {
+      removeLayerAndSource('route');
+      removeLayerAndSource('oneCar');
+      pointColor.value = ('#4264fb');
+      updateMap();
+    }
+  });
+})
 </script>
 
 <style scoped>
-#map{
+#map {
   width: 100%;
   height: 500px;
 }
 </style>
+
