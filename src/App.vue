79a3a153f13cbd6af40cf50ea69286d1b4d6dc2f
Chef1230 <109411548+Chef1230@users.noreply.github.com>
2023-10-17 15:24:43 +0800

    Add files via upload
    修改了界面，封装了一些功能

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script setup>
-//import { UploadOutlined } from '@ant-design/icons-vue';
-// import Frame from './components/Frame.vue'  //整体的框架组件
-import BaiduMap from './components/BaiduMap.vue'
+import Map from '../src/components/Map.vue'
 </script>
 
 <template>
-    <!-- <Frame></Frame> -->
-     <BaiduMap></BaiduMap>
-
-    <!--绘制文件上传按钮
-    <a-upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" directory>
-      <a-button>
-        <upload-outlined></upload-outlined>
-        Upload Directory
-      </a-button>
-    </a-upload>-->
+  <Map></Map>
 </template>
+
+<style scoped></style>
